582d172fbd1d0225fda31d283a3a86705a0cb92c
Le-Rebeu <56457826+Le-Rebeu@users.noreply.github.com>
2020-05-29 14:44:48 +0200

    Bug

--- extension.js
+++ extension.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 document.getElementById("myHead").innerHTML =
 	"<title>Dz Lakama</title>"
 	+"<h2>DZ - Le_Rebeu's Website</h2>"
@@ -34,41 +33,4 @@ document.getElementById("myHead").innerHTML =
 
 document.getElementById("myFooter").innerHTML =
 	"<p id='copyright' align='right'>Copyright &copy; " + new Date().getFullYear() + "</p>"
-=======
-document.getElementById("myHead").innerHTML =
-	"<title>Dz Lakama</title>"
-	+"<h2>DZ - Le_Rebeu's Website</h2>"
-	+		"<nav>"
-	+			"<ul class='listonglet'>"
-	+				"<li>"
-	+					"<div class='onglet'>"
-	+						"<a href='index.html'>Acceuil</a>"
-	+					"</div>"
-	+				"</li>"
-	+				"<li>"
-	+					"<div class='onglet'>"
-	+						"<a href='note.html'>Moyennes/Notes</a>"
-	+					"</div>"
-	+				"</li>"
-	+				"<li>"
-	+					"<div class='onglet'>"
-	+						"<a href='media.html'>Media</a>"
-	+					"</div>"
-	+				"</li>"
-	+				"<li>"
-	+					"<div class='onglet'>"
-	+						"<a href='agendas.html'>Agendas</a>"
-	+					"</div>"
-	+				"</li>"
-	+				"<li>"
-	+					"<div class='onglet'>"
-	+						"<a href='journal.html'>Journal</a>"
-	+					"</div>"
-	+				"</li>"
-	+			"</ul>"
-	+		"</nav>";
-
-document.getElementById("myFooter").innerHTML =
-	"<p id='copyright' align='right'>Copyright &copy; " + new Date().getFullYear() + "</p>"
->>>>>>> Stashed changes
 	+ "<p id='credits' align='right'>Site crée par Lakama Sofiène</p>";
